3836ef359092e7e81723f616d089e0567e724899
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-05-25 16:56:49 +0100

    Correct name for log_path

--- R/queue_local_worker.R
+++ R/queue_local_worker.R
@@ -10,7 +10,7 @@
 ## like the BRPOPLPUSH thing that Redis does but we won't be doing it
 ## atomically.
 queue_local_worker <- function(root, context_id=NULL, loop=TRUE, verbose=TRUE,
-                               log_dir=NULL) {
+                               log_path=NULL) {
   if (verbose) {
     context::context_log_start()
     on.exit(context::context_log_stop())
@@ -29,7 +29,7 @@ queue_local_worker <- function(root, context_id=NULL, loop=TRUE, verbose=TRUE,
   }
 
   ctx <- context::context_handle(root, context_id, db)
-  obj <- queue_local(ctx, log_dir)
+  obj <- queue_local(ctx, log_path)
 
   if (loop) {
     obj$run_loop()
