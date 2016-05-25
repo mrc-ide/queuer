c444f67e0eecae5627d4691b09ad35df718dd43e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-05-25 16:47:20 +0100

    Correctly pass log_dir through

--- R/queue_local_worker.R
+++ R/queue_local_worker.R
@@ -9,7 +9,8 @@
 ## written to just after we pop something from the queue.  This is
 ## like the BRPOPLPUSH thing that Redis does but we won't be doing it
 ## atomically.
-queue_local_worker <- function(root, context_id=NULL, loop=TRUE, verbose=TRUE) {
+queue_local_worker <- function(root, context_id=NULL, loop=TRUE, verbose=TRUE,
+                               log_dir=NULL) {
   if (verbose) {
     context::context_log_start()
     on.exit(context::context_log_stop())
@@ -28,7 +29,7 @@ queue_local_worker <- function(root, context_id=NULL, loop=TRUE, verbose=TRUE) {
   }
 
   ctx <- context::context_handle(root, context_id, db)
-  obj <- queue_local(ctx)
+  obj <- queue_local(ctx, log_dir)
 
   if (loop) {
     obj$run_loop()
