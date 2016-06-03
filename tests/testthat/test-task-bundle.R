9a3720c3adaea74a088ac2fad64ab14672ac20dc
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-06-03 09:00:20 +0100

    Fix failing test
    Not sure if this is the best behaviour, but it should be OK for now
    because nothing really relies on this.  I'm not thrilled that this
    has changed though.

--- tests/testthat/test-task-bundle.R
+++ tests/testthat/test-task-bundle.R
@@ -94,6 +94,6 @@ test_that("task_bundle, delete", {
 
   obj <- queue_local(ctx)
   x <- qlapply(1:5, list, obj)
-  expect_equal(obj$tasks_delete(x$ids), rep(TRUE, 5))
-  expect_equal(obj$tasks_delete(x$ids), rep(FALSE, 5))
+  expect_equal(obj$tasks_delete(x$ids), TRUE)
+  expect_equal(obj$tasks_delete(x$ids), FALSE)
 })
